4cf6ff34b868b224a984a5e9041c45799cde1bb9
JulianeMartins <martinsjuliane@icloud.com>
2021-03-27 15:29:50 -0300

    adding default configuration

---
+++ src/consts.js
@@ -0,0 +1,36 @@
+export const SENA = {
+  min_of_dozens: 6,
+  max_of_dozens: 15,
+  amount_of_dozens: 60,
+  prices: [
+    { amount: 6, price: 4.5 },
+    { amount: 7, price: 31.5 },
+    { amount: 8, price: 126 },
+    { amount: 9, price: 378 },
+    { amount: 10, price: 945 },
+    { amount: 11, price: 2079 },
+    { amount: 12, price: 4158 },
+    { amount: 13, price: 7722 },
+    { amount: 14, price: 13513.5 },
+    { amount: 15, price: 22522.5 },
+  ],
+};
+
+export const QUINA = {
+  min_of_dozens: 5,
+  max_of_dozens: 15,
+  amount_of_dozens: 81,
+  prices: [
+    { amount: 5, price: 2 },
+    { amount: 6, price: 12 },
+    { amount: 7, price: 42 },
+    { amount: 8, price: 112 },
+    { amount: 9, price: 252 },
+    { amount: 1, price0: 504 },
+    { amount: 1, price1: 924 },
+    { amount: 1, price2: 1584 },
+    { amount: 1, price3: 2574 },
+    { amount: 1, price4: 4004 },
+    { amount: 1, price5: 6006 },
+  ],
+};
